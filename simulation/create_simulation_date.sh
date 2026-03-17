b20ce10a9da489b684aef9c302337042ac9a7542
ktonosaki <67316927+ktonosaki@users.noreply.github.com>
2026-03-17 12:05:08 +0900

    Update create_simulation_date.sh

--- simulation/create_simulation_date.sh
+++ simulation/create_simulation_date.sh
@@ -1,16 +1,14 @@
 # Create simulation_files
 
 Rscript simulate_sites.R \
-  --contexts CG \
   --chr_len 10000000 \
-  --site_lambda_CG 20000 \
+  --site_lambda 20000 \
   --block_frac 0.05 \
   --delta_grid 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0 \
   --block_len_lo 300 --block_len_hi 2000 \
   --groups WT,MT \
   --rep_per_group 4 \
   --rho 0.3 \
-  --zeta_CHH 0.4 \
   --logcov_mu 3.0 \
   --logcov_sd 1.2 \
   --dmr_site_prop 0.5 \
